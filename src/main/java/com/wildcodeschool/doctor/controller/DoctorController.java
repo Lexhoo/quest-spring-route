41eb684bb33e7b6442a5239156c0de49acc3983d
Lexhoo <lexhoo@yahoo.fr>
2020-04-27 20:25:00 +0200

    Doctors OK

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,31 @@
 package com.wildcodeschool.doctor.controller;
 
-import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    @ResponseStatus(code = HttpStatus.OK)
+    public Person doctor(@PathVariable int number) {
+        Person response;
+        if (number < 0 || number > 13) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+        }
+        else if(number == 13){
+            response = new Person("Jodie Whittaker", 13);
+        }
+        else{
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Status 303");
+        }
+        return response;
     }
+
 }
---
+++ src/main/java/com/wildcodeschool/doctor/controller/Person.java
@@ -0,0 +1,27 @@
+package com.wildcodeschool.doctor.controller;
+
+public class Person {
+    private int number;
+    private String name;
+
+    public Person(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
